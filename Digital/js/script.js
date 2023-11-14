8a6da4a22d622247f16675e504cc24b34efaccf7
burhanuddinnur <144408619+burhanuddinnur@users.noreply.github.com>
2023-11-14 17:06:16 +0700

    Add files via upload

---
+++ Digital/js/script.js
@@ -0,0 +1,50 @@
+// toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+const waitForImages = () => {
+  const images = [...document.querySelectorAll("img")];
+  const totalImages = images.length;
+  let loadedImages = 0;
+  const loaderEl = document.querySelector(".loader span");
+
+  images.forEach((image) => {
+    imagesLoaded(image, (instance) => {
+      if (instance.isComplete) {
+        loadedImages++;
+        let loadProgress = loadedImages / totalImages;
+
+        gsap.to(loaderEl, {
+          duration: 1,
+          scaleX: loadProgress,
+          backgroundColor: `hsl(${loadProgress * 120}, 100%, 50%`,
+        });
+
+        if (totalImages == loadedImages) {
+          gsap
+            .timeline()
+            .to(".loading__wrapper", {
+              duration: 1.8,
+              opacity: 0,
+              pointerEvents: "none",
+            })
+            .call(() => init());
+        }
+      }
+    });
+  });
+};
+
+waitForImages();
